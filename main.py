6950437c932f9be7dd205c705c2edb251a2ae0fc
presence-16 <sb15289@student.samk.fi>
2024-11-21 13:47:58 +0000

    tractor is working

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from addition import addition
+from division import division
+from multiplication import multiply
+from subtraction import subtraction
+
+print("using addition")
+x = addition(1,3)
+
+print(f'1 + 3 = {x}')
+
+
+print("using subtraction")
+x = subtraction(1,3)
+
+print(f'1 - 3 = {x}')
+
+
+print("using multiplication")
+x = multiply(1,3)
+
+print(f'1 * 3 = {x}')
+
+print("using division")
+x = division(1,3)
+
+print(f'1 / 3 = {x}')
+
 
