f5ef4d05ff76f499525425a266d77a897cd9207a
presence-16 <sb15289@student.samk.fi>
2024-11-21 13:54:39 +0000

    changed

--- division.py
+++ division.py
@@ -2,6 +2,6 @@ def division(num1 , num2):
     try:
         if num2 != 0:
          return num1/num2
-    except ValueError:
+    except ZeroDivisionError:
       print('division by 0 is not allowed')
          
